8da215689d7651dc465f38f6890bd2a20e18d7f5
MarioBarrera14 <javimario69@gmail.com>
2024-03-24 22:06:03 -0300

    bloc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require('./server')
 const express = require('express');
 require('./database')
+
+
 // Iniciar servidor
 app.listen(app.get('PORT'), () => {
   console.log('Servidor escuchando en el puerto', app.get('PORT'));
