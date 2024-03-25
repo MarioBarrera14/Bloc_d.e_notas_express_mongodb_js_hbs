a4038ae4a15bb5a3b0c346a578b089648dadf9d8
MarioBarrera14 <javimario69@gmail.com>
2024-03-24 22:28:26 -0300

    bloc

--- src/database.js
+++ src/database.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 require('dotenv').config(); // Carga las variables de entorno de .env
 
 
-const MONGODB_URI = process.env.MONGODB_URI; // Obtiene la URL de la base de datos desde las variables de entorno
+// Cadena de conexión a MongoDB
+const uri = 'mongodb+srv://mario22:txbQlJTdn2mOWlMM@cluster0.6u7xir8.mongodb.net/?retryWrites=true&w=majority';
 
-// Conexión a la base de datos MongoDB
-mongoose.connect(MONGODB_URI)
-    .then(() => console.log('Conexión a MongoDB establecida'))
-    .catch(err => console.error('Error de conexión a MongoDB:', err));
+// Conexión a la base de datos
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión exitosa a la base de datos');
+  })
+  .catch(err => {
+    console.error('Error de conexión a la base de datos:', err.message);
+  });
